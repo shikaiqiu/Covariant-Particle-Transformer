1486a0287203757125b49b66301dfe44732c981d
shikai <calvin_qiu@berkeley.edu>
2023-04-17 02:06:55 -0400

    upload fiels

--- download_datasets.sh
+++ download_datasets.sh
@@ -1,6 +1,7 @@
+echo "NOTE: sometimes 'gdown' can fail, if so please download the datasets manually from link printed in the error message and resume the script."
 mkdir -vp data/final
-
-ttH=1-9w64szj-hcElSFLl6zedoyy8j10ZpfB
-gdown $ttH -O data/final/ds.zip
-# unzip 
-unzip data/final/ds.zip -d data/final/
+# download ttH dataset
+id=1-9w64szj-hcElSFLl6zedoyy8j10ZpfB
+gdown $id -O data/final/ds.zip
+unzip data/final/ds.zip -d data/final/ttH
+rm data/final/ds.zip
