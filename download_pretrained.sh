1c996b97c1e40d8db597b2e91a1e269c23450549
shikai <calvin_qiu@berkeley.edu>
2023-04-22 03:51:05 -0400

    added dataset links

--- download_pretrained.sh
+++ download_pretrained.sh
@@ -5,7 +5,7 @@ mkdir -vp "${model_dir}"
 mkdir -vp "${model_dir}/saved_models"
 # download model weights
 gdown 10z3FyIvyuVuo3zOFThTTGAb8R8H3g3rU -O "${model_dir}/saved_models/" 
-# download precomputed inference output
+# download precomputed test predictions
 gdown 1zSregpa-s8lgIIXucrg9Shkercdzguey -O "${model_dir}/" 
 gdown 16PPIq7PyMfCdsKx4hy95TVlGWWrPXChc -O "${model_dir}/"
 gdown 1-2KZURP0QmJOE2JuSMhJqYlKRqErgX4r -O "${model_dir}/"
@@ -18,5 +18,5 @@ mkdir -vp "${model_dir}"
 mkdir -vp "${model_dir}/saved_models"
 # download model weights
 gdown 18Hll5O-uboumzIrPzwyZ1YP6K4MiQ_-_ -O "${model_dir}/saved_models/"
-# download precomputed inference output
+# download precomputed test predictions
 gdown 1-BEX1iylRKsvui4vytzinguXCSONkRRG -O "${model_dir}/"
